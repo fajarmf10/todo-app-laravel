e6f6840fb0764cdc3538e2db934ad7ca15854d4a
Fajar Maulana Firdaus <fajar.firdaus@xendit.co>
2023-01-26 22:35:45 +0700

    feat: add bootstrap

--- resources/views/todo/todo.blade.php
+++ resources/views/todo/todo.blade.php
@@ -4,6 +4,11 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Todo App | An App to Help You Organize Your Task!</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('/todo/app.css') }}">
     </head>
-    <body></body>
+    <body>
+        <h1>Hi all!</h1>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
 </html>
