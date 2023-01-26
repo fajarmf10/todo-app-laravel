faf12a3548e4861d07f7419a95043199bf13478b
Fajar Maulana Firdaus <fajar.firdaus@xendit.co>
2023-01-26 23:08:02 +0700

    feat: add simple todo and their boilerplate

--- resources/views/todo/todo.blade.php
+++ resources/views/todo/todo.blade.php
@@ -8,7 +8,24 @@
         <link rel="stylesheet" href="{{ asset('/todo/app.css') }}">
     </head>
     <body>
-        <h1>Hi all!</h1>
+        <main class="wrapper d-flex flex-column align-items-center justify-content-center">
+            <form action="" class="todo-form shadow-sm">
+                <div class="form-floating mb-3">
+                    <textarea type="text" class="form-control rounded-2" id="inputTodo" aria-describedby="todoHelpText"> </textarea>
+                    <label for="inputTodo" class="form-label">Todo Item</label>
+                    <div id="todoHelpText" class="form-text">Enter your todo item above.</div>
+                </div>
+                <div class="d-grid d-md-flex justify-content-md-end">
+                    <button type="submit" class="btn btn-primary" type="button">Submit</button>
+                </div>
+            </form>
+
+            <div class="todo-list mb-3">
+                <div class="item d-flex align-items-center shadow-sm mb-3">
+                    <h2> just another h2 </h2>
+                </div>
+            </div>
+        </main>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
